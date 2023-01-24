61a4099fb28125e74ca9222b9f3cef0a661a42db
angelique-df <faye.angelique.d@gmail.com>
2023-01-24 16:19:02 +0000

    Update AffichageRectangle.java

--- src/AffichageRectangle.java
+++ src/AffichageRectangle.java
@@ -4,11 +4,10 @@ public class AffichageRectangle {
     }
     public static void dessinerRectangle(char symbole){
         int largeur, hauteur = 0;
-        System.out.println("Entrez la largeur du rectangle");
+        System.out.println("Entrez la largeur du rectangle : ");
         largeur = Terminal.lireInt();
-        System.out.println("Entrez la hauteur du rectangle");
+        System.out.println("Entrez la hauteur du rectangle : ");
         hauteur = Terminal.lireInt();
-
         for (int i = 0; i < hauteur; i++) {
             for (int j = 0; j < largeur; j++) {
                 System.out.print(symbole);
